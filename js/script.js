7599a8bc07bba7d6fc1770350b5de43f213460ee
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-12-26 16:08:17 -0300

    Add event listener to get submit value

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const form = document.querySelector('#toDo-form')
-const taskInput = document.querySelector("#task-title-input")
+const taskTitleInput = document.querySelector("#task-title-input")
 const taskButton = document.querySelector("#task-title-button")
 const toDoListUl = document.querySelector("#toDO-list")
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault() //para evitar o recarregamento da página que é a config. padrão
+    
+    const taskTitle = taskTitleInput.value
+    console.log(taskTitle)
+})
 
+// taskButton.addEventListener('click', () => {
+//     console.log('clicou')
+// }) 
 
