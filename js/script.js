b4d6be8a7ed477055cdba07d4ecca220a7521969
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-12-26 15:41:09 -0300

    Add Script - querySelector

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#toDo-form')
+const taskInput = document.querySelector("#task-title-input")
+const taskButton = document.querySelector("#task-title-button")
+const toDoListUl = document.querySelector("#toDO-list")
+
+
+
